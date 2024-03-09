1baadd136def3a777dfd585e6f14abe58ec032de
Michal Hoffman <michal.z.hoffman@gmail.com>
2024-03-09 12:32:03 +0000

    refactoring test python script

--- test.py
+++ test.py
@@ -1,33 +1,25 @@
 import json
 
-def get_test_json():
-    #  Define a Python dictionary that we'll convert to a JSON object
-    test_data = [
-        {
+test_data = [
+    {
         "severity": "HIGH",
         "message": "This error blablabla",
         "errorCode": "REENTRY_ERROR",
-        "lines": [5,10],
-        },
-        {
+        "lines": [5, 10],
+    },
+    {
         "severity": "LOW",
         "message": "This error OOOOOO WEEEEE!",
         "errorCode": "REENTRY_ERROR",
-        "lines": [2,10],
-        },
-        {
+        "lines": [2, 10],
+    },
+    {
         "severity": "HIGH",
         "message": "This error wabba lubba dub dub!",
         "errorCode": "REENTRY_ERROR",
-        "lines": [2,10],
-        }
+        "lines": [2, 10],
+    }
 ]
-    
-    # Convert the Python dictionary to a JSON string
-    json_data = json.dumps(test_data)
-    
-    # Return the JSON string
-    return json_data
 
-# Call the function and print the result
-print(get_test_json())
+# Print the test data as a JSON string
+print(json.dumps(test_data))
