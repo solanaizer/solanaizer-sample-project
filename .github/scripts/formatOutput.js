f8763e5dd6312e4ffbe5ea8a98d4398bf4d34ef8
Michal Hoffman <michal.z.hoffman@gmail.com>
2024-03-09 15:41:01 +0000

    console.tabling the output using js

---
+++ .github/scripts/formatOutput.js
@@ -0,0 +1,14 @@
+const inputChunks = [];
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', function(chunk) {
+    inputChunks.push(chunk);
+});
+
+process.stdin.on('end', function() {
+    const inputJSON = inputChunks.join();
+    const inputData = JSON.parse(inputJSON);
+    console.table(inputData);
+});
