a301f4ba253e640248108145a9cbae57cf01bf8a
Michal Hoffman <michal.z.hoffman@gmail.com>
2024-03-09 14:04:48 +0000

    testing prompt

--- ai_validator.py
+++ ai_validator.py
@@ -20,7 +20,7 @@ def analyze_vulnerability_with_gpt(api_key, file_content):
         "Authorization": f"Bearer {api_key}"
     }
 
-    prompt = f"Detect all vulnerabilities within the provided Rust smart contract code for Solana blockchain. \n '{file_content}' \n Respond in JSON format, filling the following properties for each vulnerability found \n message: Provide a detailed description of any vulnerabilities found.\n severity: Specify the severity of the vulnerability (low, medium, or high).\line: This should be an array where the first element is the line and the second is the column where the vulnerability is present.\n\n---\n"
+    prompt = f"Detect all vulnerabilities within the provided Rust smart contract code for Solana blockchain. \n '{file_content}' \n Respond in JSON format, Your responses should only be code, without explanation or formatting, filling the following properties for each vulnerability found \n message: Provide a detailed description of any vulnerabilities found.\n severity: Specify the severity of the vulnerability (low, medium, or high).\line: This should be an array where the first element is the line and the second is the column where the vulnerability is present.\n\n---\n"
 
     chat_request = ChatRequest(
         model="gpt-3.5-turbo",
